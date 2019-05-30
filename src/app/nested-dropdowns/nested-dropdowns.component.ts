36c77674af6925f095b670b9462bf03b00117cf1
Hanleyjames <hanley.doth@outlook.com>
2019-05-30 12:41:04 -0700

    removed uneccesary import of HttpClientModule in nested-dropdowns component

--- src/app/nested-dropdowns/nested-dropdowns.component.ts
+++ src/app/nested-dropdowns/nested-dropdowns.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
